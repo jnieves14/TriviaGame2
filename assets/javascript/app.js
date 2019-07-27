d707b362ccc75e2b13989999fcea3d7503bb6482
Jannah June Nieves <jannahjune@gmail.com>
2019-07-27 07:15:41 -0700

    reset button semi finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,91 +1,102 @@
 $(document).ready(function() {
     // when the page is loaded first, the timer, questions and summary must be hidden.
     
-        $('#countdown').hide();
-        $('.questionBlock').hide();
-        $('.results').hide();
+    $('#countdown').hide();
+    $('.questionBlock').hide();
+    $('.results').hide();
         
     
-        // Declare Variables
-        var number = 70; //number of seconds
-        var intervalId;
-        var correctCount = 0;
-        var wrongCount = 0;
-        var unansweredCount = 0;
-    
+    // global variables
+    var number = 70; //timer
+    var intervalId;
+    var correctCount = 0;
+    var wrongCount = 0;
+    var unansweredCount = 0;
+
     //ALL FUNCTIONS
     
-        // function to show questions
-        function showQuestions(){
-            $('#countdown').show();
-            $('.questionBlock').show();
-            $('#gameFinished').show();
-        }
+    // function to show questions
+    function showQuestions(){
+        $('#countdown').show();
+        $('.questionBlock').show();
+        $('#gameFinished').show();
+        $('#reset').hide();
+    }
     
-        // function for timer
-        function countdownTimer(){
-            intervalId = setInterval(decrement, 1000);
-        }
+    // function for timer
+    function countdownTimer(){
+        intervalId = setInterval(decrement, 1000);
+    }
        
-        // function to decrement timer
-        function decrement(){
-            number--;
-            $('#timer').html(" " + number + " " + "seconds");
-            if (number ===1){
-                $('#timer').html(" " + number + " " + "second");
-            }
-            else if(number ===0) {
-                stop();
-                hide();
-                displaySummary();
-            }
+    // function to decrement timer
+    function decrement(){
+        number--;
+        $('#timer').html(" " + number + " " + "seconds");
+        if (number ===1){
+            $('#timer').html(" " + number + " " + "second");
+        } else if(number ===0) {
+            stop();
+            hide();
+            displaySummary();
         }
+    }
     
-        //function to clear timer
-        function stop() {
-            clearInterval(intervalId);
-        }
+    //function to stop and clear timer
+    function stop() {
+        clearInterval(intervalId);
+    }
     
-            //function to hide text after questions are answered or timer out
-        function hide(){
-            $('#countdown').hide();
-            $('.questionBlock').hide();
-            $('#gameFinished').hide();
-        }
+    //function to hide text after questions are answered or timer out
+    function hide(){
+        $('#countdown').hide();
+        $('.questionBlock').hide();
+        $('#gameFinished').hide();
+    }
     
-        // function to display summary of game
-        function displaySummary(){
-            $('.results').show();
-            unansweredCount = (10-(correctCount+wrongCount));
-            $('#correctCount').text("Correct Answers:" + " " + correctCount); 
-            $('#wrongCount').text("Wrong Answers:" + " " + wrongCount); 
-            $('#unansweredCount').text("Unanswered:" + " " + unansweredCount); 
-        }
+    // function to display summary of game/ scorebaord
+    function displaySummary(){
+        $('.results').show();
+        unansweredCount = (10-(correctCount+wrongCount));
+        $('#correctNumb').text("Correct Answers:" + " " + correctCount); 
+        $('#wrongNumb').text("Wrong Answers:" + " " + wrongCount); 
+        $('#unansweredNumb').text("Unanswered:" + " " + unansweredCount); 
+        $('#reset').show();
+    }
     
     // CLICK EVENTS
     
-        //Clicking Start Button
-        $('#start').on('click', function(){
-            $('#start').hide();
-            showQuestions();
-            countdownTimer();
-        }); 
+    //Clicking Start Button
+    $('#start').on('click', function(){
+        $('#start').hide();
+        showQuestions();
+        countdownTimer();
+    }); 
     
-        //Clicking Done Button
-        $('#gameFinished').on('click', function(){
-            $('#start').hide(); 
-            hide();
-            displaySummary();
-        });
+    //Clicking Done Button
+    $('#gameFinished').on('click', function(){
+        $('#start').hide(); 
+        hide();
+        displaySummary();
+    });
+
+    //Clicking Radio buttons
+    $('input[type=radio]').on ('change', function(){
+        correctCount = $('input[value=correct]:checked').length;
+        wrongCount = $('input[value=wrong]:checked').length;
+        unansweredCount = (10-(correctCount+wrongCount));
+    });
     
+    //reset button resets the game
+    $("#reset").on("click", function() {
+        $('#start').hide();
+        $('.results').hide();
+        $('input[type=radio]').prop('checked', false);
+        showQuestions();
+        //need a way to add code so that when game play again button is clicked,
+        //the summary/scoreboard is reset
+        number = 70;
+    });
 
-        $("#reset").on("click", function() {
-            $('#start').hide();
-            $('.results').hide();
-            showQuestions();
-            number = 70;
-
-        });
 
         
     // Last closing bracket
